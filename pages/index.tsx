d2a31058ea11b155db936996c9c65ed50674122c
Rick Hausz <pinkhendrix1881@gmail.com>
2022-09-15 15:03:06 -0700

    Added our top 'div' with Logos.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from 'next';
+//Components
+import Header from '../components/Header/Header';
 
 const Home: NextPage = () => {
-  return <main>FilmsDB</main>;
+  return (
+    <main className="relative h-screen overflow-y-scroll">
+      <Header />
+      FilmsDB
+    </main>
+  );
 };
 
 export default Home;
